9fe89ebf8d6aa271893273e89de16208c05e5619
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-12-04 18:40:17 +0100

    day3 cleanup

--- src/main/kotlin/day3/day3.kt
+++ src/main/kotlin/day3/day3.kt
@@ -4,11 +4,9 @@ import java.io.File
 
 val rows = File("inputs/day3.txt").readLines()
 
-fun String.getRepeating(idx: Int): Char {
-    return this[idx % this.length]
-}
+fun String.getRepeating(idx: Int) = this[idx % this.length]
 
-val Char.isTree get() = this == '#'
+val Char.treeCount get() = if (this == '#') 1 else 0
 
 fun main() {
     partOne()
@@ -23,9 +21,7 @@ fun partTwo() {
     val slopes = listOf(1 to 1, 3 to 1, 5 to 1, 7 to 1, 1 to 2)
 
     val res = slopes.fold(1L) { acc, pair ->
-        val slo = evaluateSlope(pair.first, pair.second)
-        println(slo)
-        acc * slo
+        acc * evaluateSlope(pair.first, pair.second)
     }
     println(res)
 }
@@ -33,9 +29,9 @@ fun partTwo() {
 fun evaluateSlope(right: Int, down: Int): Int {
     var idx = 0
     var trees = 0
-    for (r in rows.withIndex()) {
-        if (r.index % down != 0) continue
-        if (r.value.getRepeating(idx).isTree) trees++
+
+    for (r in rows.chunked(down) { it.first() }) {
+        trees += r.getRepeating(idx).treeCount
         idx += right
     }
     return trees
