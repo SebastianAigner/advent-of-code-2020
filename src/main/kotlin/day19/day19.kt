e4154cb602092b057c61971cb6f60682ebe628c7
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-12-21 16:37:03 +0100

    day19.2

--- src/main/kotlin/day19/day19.kt
+++ src/main/kotlin/day19/day19.kt
@@ -12,6 +12,34 @@ val rules = File("inputs/day19.txt").readLines().takeWhile { it.isNotBlank() }.s
 val testStrings = File("inputs/day19.txt").readLines().takeLastWhile { it.isNotBlank() }
 
 fun main() {
+
+}
+
+fun partTwo() {
+    val pumpedRegexes = mutableListOf<Regex>()
+    for (pump42 in 0..10) {
+        for (pump4231 in 0..10) {
+            val adjustedRules = rules
+                .toMutableMap().apply {
+                    val string42 = (0..pump42).map { "42" }.joinToString(" ")
+                    set(8, string42 /* with [42] being pumped */)
+
+                    val fortyTwos = (0..pump4231).map { "42" }
+                    val thirtyOnes = (0..pump4231).map { "31" }
+                    val x = (fortyTwos + thirtyOnes).joinToString(" ")
+                    set(11, "42 31 | $x" /* with [42 31] being pumped*/)
+                }.toMap()
+            val aRegex = adjustedRules.parseSingleRule(adjustedRules[0]!!)
+            println(aRegex)
+            pumpedRegexes.add(aRegex.toRegex())
+        }
+    }
+    println(testStrings.count { testString ->
+        pumpedRegexes.any { it.matches(testString) }
+    })
+}
+
+fun partOne() {
     val regex = rules.parseSingleRule(rules[0]!!)
     println(regex)
     val tester = regex.toRegex()
@@ -28,8 +56,8 @@ fun Map<Int, String>.parseSingleRule(singleRule: String): String {
     }
     if (singleRule.contains(" ")) {
         // it's a concat rule
-        val (first, second) = singleRule.split(" ")
-        return "${parseSingleRule(first)}${parseSingleRule(second)}"
+        val rules = singleRule.split(" ")
+        return rules.joinToString("") { parseSingleRule(it) }
     }
     if (singleRule.contains("\"")) {
         // it's a single-char rule
